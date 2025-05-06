1cd6f84aacdb50a9e017c2c1f35f6dfbf8665fb6
Kevin2li <kevin2li@qq.com>
2025-05-06 14:01:00 +0800

    update

--- apsdai.js
+++ apsdai.js
@@ -1,1 +1,4 @@
-window.Apsdai = { name: 'jkme8qm7ox' };
+window.Apsdai = Object.assign(window.Apsdai || {}, { name: 'jkme8qm7ox' })
+window.Apsdai = Object.assign(window.Apsdai || {}, { hk_name: 'jkme8qm7ox' });
+
+// window.Apsdai = { name: 'jkme8qm7ox' };
